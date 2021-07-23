20ae9401bd6eb30961e88c235a0bb13bc825e3af
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-07-24 01:56:17 +0530

    feat: level 1 wip
    Added:
    - level layout
    - player
    - keypress event
    - game map
    - level label
    - score label

--- src/game.js
+++ src/game.js
@@ -1,13 +1,98 @@
+const MOVE_SPEED = 120;
+const JUMP_FORCE = 360;
+
 kaboom({
   global: true,
   fullscreen: true,
   scale: 1,
   debug: true,
-  clearColor: [0, 0, 0, 1]
+  clearColor: [0, 0, 0, 1],
 });
 
-scene("game", () => {
+// Load sprites
+loadRoot('/assets/');
+loadSprite('coin', 'coin.png');
+loadSprite('evil-shroom', 'evil-shroom.png');
+loadSprite('brick', 'brick.png');
+loadSprite('block', 'block.png');
+loadSprite('mario', 'mario.png');
+loadSprite('mushroom', 'mushroom.png');
+loadSprite('surprise', 'surprise.png');
+loadSprite('unboxed', 'unboxed.png');
+loadSprite('pipe-top-left', 'pipe-top-left.png');
+loadSprite('pipe-top-right', 'pipe-top-right.png');
+loadSprite('pipe-bottom-left', 'pipe-bottom-left.png');
+loadSprite('pipe-bottom-right', 'pipe-bottom-right.png');
 
+scene('game', () => {
+  // Setup layers
+  layers(['bg', 'obj', 'ui'], 'obj');
+
+  const map = [
+    '                                         ',
+    '                                         ',
+    '                                         ',
+    '                                         ',
+    '                                         ',
+    '                                         ',
+    '                                         ',
+    '     %   =*=%=                           ',
+    '                                         ',
+    '                            -+           ',
+    '                     ^   ^  ()           ',
+    '===============================    ======',
+  ];
+
+  const levelCfg = {
+    width: 20,
+    height: 20,
+    '=': [sprite('block'), solid()],
+    $: [sprite('coin')],
+    '%': [sprite('surprise'), solid(), 'coin-surprise'],
+    '*': [sprite('surprise'), solid(), 'mushroom-surprise'],
+    '}': [sprite('unboxed'), solid()],
+    '(': [sprite('pipe-bottom-left'), solid(), scale(0.5)],
+    ')': [sprite('pipe-bottom-right'), solid(), scale(0.5)],
+    '-': [sprite('pipe-top-left'), solid(), scale(0.5)],
+    '+': [sprite('pipe-top-right'), solid(), scale(0.5)],
+    '^': [sprite('evil-shroom'), solid()],
+    '#': [sprite('mushroom'), solid()],
+  };
+
+  const gameLevel = addLevel(map, levelCfg);
+
+  const scoreLabel = add([
+    text('test'),
+    pos(30, 6),
+    layer('ui'),
+    {
+      value: 'test',
+    },
+  ]);
+
+  const levelLabel = add([text('level' + 'test', pos(4, 6))]);
+
+  const player = add([
+    sprite('mario'),
+    solid(),
+    pos(30, 0),
+    body(),
+    origin('bot'),
+  ]);
+
+  keyDown('left', () => {
+    player.move(-MOVE_SPEED, 0);
+  });
+
+  keyDown('right', () => {
+    player.move(MOVE_SPEED, 0);
+  });
+
+  keyDown('space', () => {
+    if (player.grounded()) {
+      player.jump(JUMP_FORCE);
+    }
+  });
 });
 
-start("game");
+start('game');
