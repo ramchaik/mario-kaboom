4ac18fbb4f39202e9773154cbc4029af3c4b00fb
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-07-24 12:13:31 +0530

    feat: complete level one

--- game.js
+++ game.js
@@ -1,5 +1,11 @@
 const MOVE_SPEED = 120;
+const ENEMY_SPEED = 20;
 const JUMP_FORCE = 360;
+const BIG_JUMP_FORCE = 550;
+const FALL_DEATH = 400;
+
+let CURRENT_JUMP_FORCE = JUMP_FORCE;
+let isJumping = false;
 
 kaboom({
   global: true,
@@ -24,7 +30,7 @@ loadSprite('pipe-top-right', 'pipe-top-right.png');
 loadSprite('pipe-bottom-left', 'pipe-bottom-left.png');
 loadSprite('pipe-bottom-right', 'pipe-bottom-right.png');
 
-scene('game', () => {
+scene('game', ({ level, score }) => {
   // Setup layers
   layers(['bg', 'obj', 'ui'], 'obj');
 
@@ -47,30 +53,30 @@ scene('game', () => {
     width: 20,
     height: 20,
     '=': [sprite('block'), solid()],
-    $: [sprite('coin')],
+    $: [sprite('coin'), 'coin'],
     '%': [sprite('surprise'), solid(), 'coin-surprise'],
     '*': [sprite('surprise'), solid(), 'mushroom-surprise'],
     '}': [sprite('unboxed'), solid()],
     '(': [sprite('pipe-bottom-left'), solid(), scale(0.5)],
     ')': [sprite('pipe-bottom-right'), solid(), scale(0.5)],
-    '-': [sprite('pipe-top-left'), solid(), scale(0.5)],
-    '+': [sprite('pipe-top-right'), solid(), scale(0.5)],
-    '^': [sprite('evil-shroom'), solid()],
-    '#': [sprite('mushroom'), solid()],
+    '-': [sprite('pipe-top-left'), solid(), scale(0.5), 'pipe'],
+    '+': [sprite('pipe-top-right'), solid(), scale(0.5), 'pipe'],
+    '^': [sprite('evil-shroom'), solid(), 'dangerous'],
+    '#': [sprite('mushroom'), solid(), 'mushroom', body()],
   };
 
   const gameLevel = addLevel(map, levelCfg);
 
   const scoreLabel = add([
-    text('test'),
+    text(score),
     pos(30, 6),
     layer('ui'),
     {
-      value: 'test',
+      value: score,
     },
   ]);
 
-  const levelLabel = add([text('level' + 'test', pos(4, 6))]);
+  const levelLabel = add([text('Level ' + parseInt(level + 1)), pos(50, 6)]);
 
   function big() {
     let timer = 0;
@@ -90,15 +96,17 @@ scene('game', () => {
       },
       smallify() {
         this.scale = vec2(1);
+        CURRENT_JUMP_FORCE = JUMP_FORCE;
         timer = 0;
         isBig = false;
       },
       biggify(time) {
         this.scale = vec2(2);
+        CURRENT_JUMP_FORCE = BIG_JUMP_FORCE;
         timer = time;
         isBig = true;
       },
-    }
+    };
   }
 
   const player = add([
@@ -110,6 +118,67 @@ scene('game', () => {
     origin('bot'),
   ]);
 
+  action('mushroom', (m) => {
+    m.move(30, 0);
+  });
+
+  action('dangerous', (d) => {
+    d.move(-ENEMY_SPEED, 0);
+  });
+
+
+  player.action(() => {
+    camPos(player.pos);
+    if (player.pos.y >= FALL_DEATH) {
+      go('lose', { score: scoreLabel.value });
+    }
+  });
+
+  player.on('headbump', (obj) => {
+    if (obj.is('coin-surprise')) {
+      gameLevel.spawn('$', obj.gridPos.sub(0, 1));
+      destroy(obj);
+      gameLevel.spawn('}', obj.gridPos.sub(0, 0));
+    }
+    if (obj.is('mushroom-surprise')) {
+      gameLevel.spawn('#', obj.gridPos.sub(0, 1));
+      destroy(obj);
+      gameLevel.spawn('}', obj.gridPos.sub(0, 0));
+    }
+  });
+
+  player.collides('mushroom', (m) => {
+    destroy(m);
+    player.biggify(6);
+  });
+
+  player.collides('coin', (c) => {
+    destroy(c);
+    scoreLabel.value++;
+    scoreLabel.text = scoreLabel.value;
+  });
+
+  player.collides('dangerous', (d) => {
+    if (isJumping) {
+      destroy(d);
+    } else {
+      go('lose', { score: scoreLabel.value });
+    }
+  });
+
+  player.action(() => {
+    if (player.grounded()) {
+      isJumping = false;
+    }
+  });
+
+
+  player.collides('pipe', () => {
+    keyPress('down', () => {
+      go('game', { level: level + 1, score: scoreLabel.value });
+    });
+  });
+
   keyDown('left', () => {
     player.move(-MOVE_SPEED, 0);
   });
@@ -120,9 +189,14 @@ scene('game', () => {
 
   keyDown('space', () => {
     if (player.grounded()) {
-      player.jump(JUMP_FORCE);
+      isJumping = true;
+      player.jump(CURRENT_JUMP_FORCE);
     }
   });
 });
 
-start('game');
+scene('lose', ({ score }) => {
+  add([text(score, 32), origin('center'), pos(width() / 2, height() / 2)]);
+});
+
+start('game', { level: 0, score: 0 });
